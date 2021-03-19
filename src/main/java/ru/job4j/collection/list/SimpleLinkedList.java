2d6dae532b47ac76acdbbb2053fef08d7ac577f4
ainz <ainz713@gmail.com>
2021-03-19 23:42:09 +0300

    2. Создать контейнер на базе связанного списка [#493919] корректировка

--- src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -65,4 +65,16 @@ public class SimpleLinkedList<E> implements List<E> {
             }
         };
     }
+
+    public class Node<E> {
+        private  E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
