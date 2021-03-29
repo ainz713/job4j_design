896071053e377be803f3321e27d5dec535e649f4
ainz <ainz713@gmail.com>
2021-03-30 00:56:06 +0300

    0.1. FileOutputStream. [#493965] корректировка

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -6,21 +6,21 @@ public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write("1 * 1 = 1".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 2 = 2".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 3 = 3".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 4 = 4".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 5 = 5".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 6 = 6".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 7 = 7".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 8 = 8".getBytes());
-            out.write("\n".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1 * 9 = 9".getBytes());
         } catch (Exception e) {
             e.printStackTrace();
