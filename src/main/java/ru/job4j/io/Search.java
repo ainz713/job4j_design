2f5fa17241d0a80cd9777cfc5055e9e068262733
ainz <ainz713@gmail.com>
2021-04-03 12:17:40 +0300

    5. Валидация параметров запуска. [#493964]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,8 +8,12 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:\\Lexino\\");
-        search(start, "jpg").forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String condition) throws IOException {
