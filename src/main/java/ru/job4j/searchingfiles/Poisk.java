7f5e2c9dae2bc49e0b8410940fde0e22c7058d0f
ainz <ainz713@gmail.com>
2021-04-14 23:47:16 +0300

    Поиск файлов по критерию [#493984] корректировка

--- src/main/java/ru/job4j/searchingfiles/Poisk.java
+++ src/main/java/ru/job4j/searchingfiles/Poisk.java
@@ -5,59 +5,43 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.regex.Matcher;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class Poisk {
-    public void writeFiles(List<Path> sources, Path target) {
+    public static void writeFiles(List<Path> sources, Path target) {
         try (PrintWriter printWriter = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(target.toString())))) {
             for (Path fileFrom: sources) {
-                printWriter.write(fileFrom.toString() + "\n");
+                printWriter.println(fileFrom.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static List<Path> searchM(Path root, String condition) throws IOException {
-      SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(condition));
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
+    private static Predicate<Path> searchCondition(Argsname args) {
+        if (args.get("t").equals("mask")) {
+            return p -> p.toFile().getName().endsWith(args.get("n"));
+        } else if (args.get("t").equals("name")) {
+            return p -> p.toFile().getName().equals(args.get("n"));
+        } else if (args.get("t").equals("regex")) {
+            Pattern pattern = Pattern.compile(args.get("n"));
+            return p -> pattern.matcher(p.toFile().getName()).find();
+        }
+        return null;
     }
 
-    public static List<Path> searchF(Path root, String condition) throws IOException {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().equals(condition));
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
-    }
-
-    public static List<Path> searchR(Path root, String condition) throws IOException {
-        Pattern pattern = Pattern.compile(condition);
-        Matcher matcher;
-        SearchFiles searcher = new SearchFiles(p -> pattern.matcher(p.toFile().getName()).find());
+    public static List<Path> search(Path root, Argsname args) throws IOException {
+        SearchFiles searcher = new SearchFiles(Poisk.searchCondition(args));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
     public static void main(String[] args) throws IOException {
         Argsname jvm = Argsname.of(args);
-        if (jvm.get("t").equals("mask")) {
-            new Poisk().writeFiles(
-                    Poisk.searchM(Path.of(jvm.get("d")), jvm.get("n")),
-                    Paths.get(jvm.get("o"))
-            );
-        } else if (jvm.get("t").equals("name")) {
-            new Poisk().writeFiles(
-                    Poisk.searchF(Path.of(jvm.get("d")), jvm.get("n")),
-                    Paths.get(jvm.get("o"))
-            );
-        } else if (jvm.get("t").equals("regex")) {
-            new Poisk().writeFiles(
-                    Poisk.searchR(Path.of(jvm.get("d")), jvm.get("n")),
-                    Paths.get(jvm.get("o"))
-            );
-        }
+        Poisk.writeFiles(Poisk.search(Path.of(jvm.get("d")), jvm),
+                Paths.get(jvm.get("o")));
     }
 }
