cc08bacbf75e44a05dd9dd21378f994a0199d4ab
ainz <ainz713@gmail.com>
2021-05-25 22:13:16 +0300

    1. Хранилище продуктов [#852 #159857] исправление 2

--- src/main/java/ru/job4j/lsp/ControllQuality.java
+++ src/main/java/ru/job4j/lsp/ControllQuality.java
@@ -4,14 +4,24 @@ import java.util.List;
 
 public class ControllQuality {
 
+    private Strategy strategy;
+
     public ControllQuality() {
+    }
+
+    public ControllQuality(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean executeStrategy(Food food) {
+        return strategy.add(food);
     }
 
     public void whatStorage(List<Strategy> strategy, Food food) {
         for (Strategy e
                 :strategy) {
             if (e.accept(food)) {
-                new Context(e).executeStrategy(food);
+                new ControllQuality(e).executeStrategy(food);
             }
         }
     }
