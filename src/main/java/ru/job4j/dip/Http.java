082dbddb670234c0a36ea304eb2e5ed69d2b80dd
ainz <ainz713@gmail.com>
2021-06-01 00:09:32 +0300

    0. Видео [#4917] DIP

---
+++ src/main/java/ru/job4j/dip/Http.java
@@ -0,0 +1,10 @@
+package ru.job4j.dip;
+
+public class Http {
+
+    private XMLHttpService xmlHttpService;
+
+    public Http(XMLHttpService xmlHttpService) {
+        this.xmlHttpService = xmlHttpService;
+    }
+}
